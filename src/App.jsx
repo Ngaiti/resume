f5e2944b1fe41e05a65ee7dfd28025917f67ac1a
Ngaiti <henfanar@gmail.com>
2023-05-30 11:58:44 +0800

    Experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 function App() {
   return (
     <>
-      <h1>Kevin Yap </h1>
+      <h1>Kevin Yap</h1>
+      <h1></h1>
       <h2>Software Developer</h2>
+      <Experience />
+    </>
+  )
+}
+
+function Experience() {
+  return (
+    <>
+      <h3>Software Developer</h3>
+      <h4>Sigma Lab Sdn Bhd</h4>
     </>
   )
 }
